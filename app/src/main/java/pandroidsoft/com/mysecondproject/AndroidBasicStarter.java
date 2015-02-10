2537f99ee440e047d8ac89000a659b06b15e7bed
pumba30 <pumba303030@yandex.ru>
2015-02-10 20:08:32 +0200

    add uses-sdk

---
+++ app/src/main/java/pandroidsoft/com/mysecondproject/AndroidBasicStarter.java
@@ -0,0 +1,44 @@
+package pandroidsoft.com.mysecondproject;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by pumba30 on 10.02.2015.
+ */
+
+
+// здесь создаим запуск тестовых активностей
+public class AndroidBasicStarter extends ListActivity {
+    String tests[] = {"LifeCycleTest", "SingleTouchTest", "MultiTouchTest", "KeyTest",
+            "AccelerometrTest", "AssetsTest", " ExternalStorageTest", "SoundPoolTest",
+            "MediaPlayerTest", "FullScreenTest", "RenderViewTest", "ShapeTest",
+            "BitmapTest", "FontTest", "SurfaceViewTest"};
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, tests));
+    }
+
+    @Override
+    protected void onListItemClick(ListView list, View view, int position,
+                                   long id) {
+        super.onListItemClick(list, view, position, id);
+        String testName = tests[position];
+
+        try {
+            Class clazz = Class.forName("pandroidsoft.com.mysecondproject." + testName);
+            Intent intent = new Intent(this, clazz);
+            startActivity(intent);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
